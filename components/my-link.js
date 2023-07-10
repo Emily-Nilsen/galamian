385ceb1087c18253af2bf37d07688157d91e38ce
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-07-10 16:03:20 +0200

    my-link

--- components/my-link.js
+++ components/my-link.js
@@ -2,7 +2,7 @@ import { forwardRef } from 'react';
 import Link from 'next/link';
 import { Menu } from '@headlessui/react';
 
-const MyLink = forwardRef((props, ref) => {
+const MyLink = forwardRef(function MyLink(props, ref) {
   let { href, children, ...rest } = props;
   return (
     <Link href={href}>
